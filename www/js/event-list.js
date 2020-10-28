b44601f530e9c6f30e05437329bb78d2c1ad7946
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-10-28 21:13:09 +0900

    イベント一覧画面のパスを修正

--- www/js/event-list.js
+++ www/js/event-list.js
@@ -1,41 +1,41 @@
-$(function () {
-  let cardDom = $('.card-rink')
-  $.ajax({
-    url: 'http://192.168.137.1:8080/api/event/eventlist.php?', //送信先
-    type: 'GET', //送信方法
-    datatype: 'json', //受け取りデータの種類
-    data: searchUrlGenerater()
-  })
-    .done(function (response) {
-      eventCardInfo = response.data
-
-      if (eventCardInfo.length > 0) {
-        eventCardInfo.forEach(function (cardItem) {
-          cardDom = cardDom.clone()
-          console.log(cardDom)
-          cardDom.attr(
-            'href',
-            '/www/event-list/detail/index.html?event-id=' + cardItem.event_id
-          )
-          cardDom
-            .find('img')
-            .attr('src', 'http://192.168.137.1:8080' + cardItem.image)
-          cardDom.find('.card-body .held-date').text(cardItem.held_date)
-          cardDom.find('.card-body .event-name').text(cardItem.event_name)
-          cardDom.find('.card-body .map').text(cardItem.map)
-          cardDom.find('.user-info p').text(cardItem.organizer)
-          cardDom
-            .find('.user-info img')
-            .attr('src', 'http://192.168.137.1:8080/' + cardItem.icon)
-          cardDom.show()
-          $('.card-columns').append(cardDom)
+$(function() {
+    let cardDom = $('.card-rink')
+    $.ajax({
+            url: 'http://192.168.137.1:8080/api/event/eventlist.php?', //送信先
+            type: 'GET', //送信方法
+            datatype: 'json', //受け取りデータの種類
+            data: searchUrlGenerater()
         })
-      } else {
-        $('.card-columns .no-event').show()
-      }
-    })
-    .fail(function (response) {
-      console.log('通信失敗')
-      console.log(response)
-    })
+        .done(function(response) {
+            eventCardInfo = response.data
+
+            if (eventCardInfo.length > 0) {
+                eventCardInfo.forEach(function(cardItem) {
+                    cardDom = cardDom.clone()
+                    console.log(cardDom)
+                    cardDom.attr(
+                        'href',
+                        'detail/index.html?event-id=' + cardItem.event_id
+                    )
+                    cardDom
+                        .find('img')
+                        .attr('src', 'http://192.168.137.1:8080' + cardItem.image)
+                    cardDom.find('.card-body .held-date').text(cardItem.held_date)
+                    cardDom.find('.card-body .event-name').text(cardItem.event_name)
+                    cardDom.find('.card-body .map').text(cardItem.map)
+                    cardDom.find('.user-info p').text(cardItem.organizer)
+                    cardDom
+                        .find('.user-info img')
+                        .attr('src', 'http://192.168.137.1:8080/' + cardItem.icon)
+                    cardDom.show()
+                    $('.card-columns').append(cardDom)
+                })
+            } else {
+                $('.card-columns .no-event').show()
+            }
+        })
+        .fail(function(response) {
+            console.log('通信失敗')
+            console.log(response)
+        })
 })
